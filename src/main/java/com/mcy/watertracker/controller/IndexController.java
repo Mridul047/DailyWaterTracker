2c971cab5582505e75b4a1d17b47a5f2138c5802
Mridul Choudhary <mridul.choudhary7@gmail.com>
2023-09-08 16:25:21 +0530

    Refactor code to support findAll.

--- src/main/java/com/mcy/watertracker/controller/IndexController.java
+++ src/main/java/com/mcy/watertracker/controller/IndexController.java
@@ -5,9 +5,13 @@ import com.mcy.watertracker.model.dto.WaterConsumptionDTO;
 import com.mcy.watertracker.service.PersonService;
 import com.mcy.watertracker.service.WaterConsumptionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class IndexController {
@@ -24,5 +28,10 @@ public class IndexController {
   public WaterConsumptionDTO registerWaterConsumption(
       @RequestBody WaterConsumptionDTO waterConsumption) {
     return waterConsumptionService.registerWaterConsumption(waterConsumption);
+  }
+
+  @GetMapping("/waterConsumption")
+  public List<WaterConsumptionDTO> getWaterConsumption(@RequestParam Long personId) {
+    return waterConsumptionService.fetchWaterConsumptionById(personId);
   }
 }
--- src/main/java/com/mcy/watertracker/service/WaterConsumptionService.java
+++ src/main/java/com/mcy/watertracker/service/WaterConsumptionService.java
@@ -6,9 +6,12 @@ import com.mcy.watertracker.model.dto.WaterConsumptionDTO;
 import com.mcy.watertracker.respository.PersonRepository;
 import com.mcy.watertracker.respository.WaterConsumptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WaterConsumptionService {
@@ -19,31 +22,46 @@ public class WaterConsumptionService {
 
   public WaterConsumptionDTO registerWaterConsumption(WaterConsumptionDTO waterConsumptionDTO) {
 
-    try {
-      WaterConsumption waterConsumption = new WaterConsumption();
-      waterConsumption.setBrandName(waterConsumptionDTO.getBrandName());
-      waterConsumption.setWaterConsumedAt(LocalDateTime.now());
-      waterConsumption.setCapacityConsumedInMl(waterConsumptionDTO.getCapacityConsumedInMl());
+    WaterConsumption waterConsumption = new WaterConsumption();
+    waterConsumption.setBrandName(waterConsumptionDTO.getBrandName());
+    waterConsumption.setWaterConsumedAt(LocalDateTime.now());
+    waterConsumption.setCapacityConsumedInMl(waterConsumptionDTO.getCapacityConsumedInMl());
 
-      // Fetch the associated person from the database or use the personId in DTO to set the
-      // association
-      Person person = personRepository.findById(waterConsumptionDTO.getPersonId()).orElse(null);
-      if (person != null) {
-        waterConsumption.setPerson(person);
-        var updatedWCR = waterConsumptionRepository.save(waterConsumption);
-        return new WaterConsumptionDTO(
-            updatedWCR.getBrandName(),
-            updatedWCR.getWaterConsumedAt(),
-            updatedWCR.getCapacityConsumedInMl(),
-            updatedWCR.getPerson().getPersonId());
-      }
+    // Fetch the associated person from the database or use the personId in DTO to set the
+    // association
+    Person person = personRepository.findById(waterConsumptionDTO.getPersonId()).orElse(null);
 
-    } catch (Exception e) {
-      e.printStackTrace();
+    if (person != null) {
+
+      waterConsumption.setPerson(person);
+      var updatedWCR = waterConsumptionRepository.save(waterConsumption);
+
+      return new WaterConsumptionDTO(
+          updatedWCR.getBrandName(),
+          updatedWCR.getWaterConsumedAt(),
+          updatedWCR.getCapacityConsumedInMl(),
+          updatedWCR.getPerson().getPersonId());
     }
-    return null;
+    return new WaterConsumptionDTO();
   }
 
   // Other service methods for water consumption (e.g., getWaterConsumptionById,
   // updateWaterConsumption, deleteWaterConsumption)
+
+  public List<WaterConsumptionDTO> fetchWaterConsumptionById(Long personId) {
+    Pageable firstPageWithTwoElements = PageRequest.of(0, 5);
+    var page = waterConsumptionRepository.findAll(firstPageWithTwoElements);
+    return page.stream()
+        .map(
+            waterConsumption -> {
+              var waterConsumptionDTO = new WaterConsumptionDTO();
+              waterConsumptionDTO.setBrandName(waterConsumption.getBrandName());
+              waterConsumptionDTO.setWaterConsumedAt(waterConsumption.getWaterConsumedAt());
+              waterConsumptionDTO.setPersonId(waterConsumption.getPerson().getPersonId());
+              waterConsumptionDTO.setCapacityConsumedInMl(
+                  waterConsumption.getCapacityConsumedInMl());
+              return waterConsumptionDTO;
+            })
+        .collect(Collectors.toList());
+  }
 }
